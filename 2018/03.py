07f6962abef3d7cc1816cec1b73b76ee8872e8d9
Trif <trif@live.dk>
2018-12-04 01:13:04 +0100

    Day 3: Use named tuple instead of class for positions

--- 2018/03.py
+++ 2018/03.py
@@ -1,5 +1,5 @@
 import re
-from collections import Counter
+from collections import Counter, namedtuple
 
 from aocd import data
 
@@ -7,30 +7,27 @@ from aocd import data
 class Claim:
     def __init__(self, definition):
         self.id, self.x, self.y, self.width, self.height = [int(i) for i in re.findall('\d+', definition)]
-        self.positions = set()
+        self.positions = []
 
 
-class ClaimedPosition:
-    def __init__(self, x, y, claim):
-        self.x, self.y, self.claim = x, y, claim
-        self.claim.positions.add(self)
-
+ClaimedPosition = namedtuple('ClaimedPosition', ['x', 'y'])
 
 claims = [Claim(line) for line in data.splitlines()]
 
 
 # A
 
-claimed_positions = set()
+claimed_positions = []
 
 for claim in claims:
     for i in range(claim.width):
         for j in range(claim.height):
-            p = ClaimedPosition(claim.x + i, claim.y + j, claim)
-            claimed_positions.add(p)
+            p = ClaimedPosition(claim.x + i, claim.y + j)
+            claim.positions.append(p)
+            claimed_positions.append(p)
 
-claimed_positions_counter = Counter((p.x, p.y) for p in claimed_positions)
-contested_square_inches = sum(1 for i in claimed_positions_counter.values() if i > 1)
+claimed_positions_counter = Counter(claimed_positions)
+contested_square_inches = sum(1 for count in claimed_positions_counter.values() if count > 1)
 
 print(contested_square_inches)
 
